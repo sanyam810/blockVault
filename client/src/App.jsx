5cbbaf8746c0ebf8871a8a6046a570eaeb2ccc6d
sanyam810 <sanyamsaini081@gmail.com>
2023-04-09 00:24:02 +0530

    just setting up a blank paper with 5 components xD

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,11 @@ const App=()=> {
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
         <Navbar />
+        <Welcome/>
       </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
     </div>
     
   )

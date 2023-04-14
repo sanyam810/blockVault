a004b62f8acf8f3ceb309bc3bab1076b390354d7
sanyam810 <sanyamsaini081@gmail.com>
2023-04-15 02:46:26 +0530

    react-app has everything it needs to interact with the smart_contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/zFdXCdoIYPwPkPLSauUZhUQJ9y5rsfUz
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.18",
-};
+require('@nomiclabs/hardhat-waffle');
+
+module.exports={
+  solidity:'0.8.0',
+  networks:{
+    sepolia:{
+      url: 'https://eth-sepolia.g.alchemy.com/v2/FKhcheg2BnTO4wKSfXzaNWj6B1YyPmpA',
+      accounts:['2b23be9c1bd64821e6d4ad7f208fd120ecafcab11001c0c530e16ce9730b2498']
+    }
+  }
+}

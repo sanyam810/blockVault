d46aeb2d6a3eab3bc2aaef7a02fb478b3d80a3df
sanyam810 <sanyamsaini081@gmail.com>
2023-05-20 22:20:30 +0530

    Imported footer image

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,14 @@
+import logo from '../../images/logo.png';
+
 const Footer=()=>{
     return (
-        <h1>Footer</h1>
+        <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+            <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+                <div>
+                    <img src={logo} alt="logo" className="w-32" />
+                </div>
+            </div>
+        </div>
     );
 }
 

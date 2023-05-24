2e529a5f802720b3ed9c35cba35b1ca9e448ceed
sanyam810 <sanyamsaini081@gmail.com>
2023-05-24 22:10:05 +0530

    implemented method to check if transactions exists

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -62,6 +62,19 @@ export const TransactionProvider=({children})=>{
             }
     }
 
+
+    const chechIfTransactionsExist=async()=>{
+        try {
+            const transactionContract=getEthereumContract();
+            const transactionCount=await transactionContract.getTransactionCount();
+
+            window.localStorage.setItem("transactionCount",transactionCount);
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object.");
+        }
+    }
+
     const connectWallet=async()=>{
         try{
             if(!ethereum)return alert("Please alert Metamask");
@@ -112,6 +125,7 @@ export const TransactionProvider=({children})=>{
 
     useEffect(()=>{
         checkIfWalletIsConnected();
+        chechIfTransactionsExist();
     },[]);
 
     return(

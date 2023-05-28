421f30f6ed2ebf3b57276b4b73eda1f891ff3a3b
sanyam810 <sanyamsaini081@gmail.com>
2023-05-28 06:32:10 +0530

    gifs are not showing with the transactions

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -52,6 +52,7 @@ export const TransactionProvider=({children})=>{
                 addressTo: transaction.receiver,
                 addressFrom: transaction.sender,
                 timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+                message: transaction.message,
                 keyword: transaction.keyword,
                 amount: parseInt(transaction.amount._hex) / (10**18)
             }));
@@ -139,6 +140,7 @@ export const TransactionProvider=({children})=>{
             
             const transactionCount=await transactionContract.getTransactionCount();
             setTransactionCount(transactionCount.toNumber());
+            window.reload();
 
         }catch(error){
             console.log(error);
